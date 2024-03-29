7ed9a3b7a14e05516bcaef3507e86509c2e780fa
Mutuku1998 <martinmutuku1998@gmail.com>
2024-03-29 20:26:18 +0300

    map

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap() {
+    var options = {
+        center: {lat: 0.0236, lng: 37.9062},
+        zoom: 8
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}
