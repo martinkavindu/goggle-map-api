b5bc6350309b1ab8edd41e0d1df807eb1a1b3095
Mutuku1998 <martinmutuku1998@gmail.com>
2024-03-30 13:59:09 +0300

    m

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById("map"), options);
 
-    google.maps.event.addListener(map,"click",(event) =>{
-        addMarker({location:event.latLng});
-    })
+    // google.maps.event.addListener(map,"click",(event) =>{
+    //     addMarker({location:event.latLng});
+    // })
 
     // markers array
 
@@ -27,42 +27,6 @@ function initMap() {
 
      }
 
-    // var marker = new google.maps.marker.AdvancedMarkerElement({
-    //     position: {lat: 3.5107, lng: 39.9093},
-    //     map: map,
-    //     icon: "location.png"
-    // });
-    //   new google.maps.Marker({
-    //     position: {lat:-3.5107, lng: 39.9093},
-    //     map,
-    //     icon: "https://img.icons8.com/nolan/2x/marker.png",
-    //   });
-
-//     const contentString =
-//     '<div id="content">' +
-//     '<div id="siteNotice">' +
-//     "</div>" +
-//     '<h1 id="firstHeading" class="firstHeading">Coming soon</h1>' +
-//     '<div id="bodyContent">' +
-//     '<p> We dont offer Unlimited Internet in your area at present, send us your details and we will contact you when it is available</p>' +
-//     "</div>" +
-//     "</div>";
-//   const infowindow = new google.maps.InfoWindow({
-//     content: contentString,
-//     ariaLabel: "Welcome",
-//   });
-//   const marker = new google.maps.Marker({
-//     position: {lat:-3.5107, lng: 39.9093},
-//     map,
-//     icon: "https://img.icons8.com/nolan/2x/marker.png",
-//   });
-
-//   marker.addListener("click", () => {
-//     infowindow.open({
-//       anchor: marker,
-//       map,
-//     });
-//   });
 function addMarker(props){
   const marker = new google.maps.Marker({
    position:props.location,
