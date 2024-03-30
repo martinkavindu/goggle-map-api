c47b494a433439f8eaab58a6bea5dec18a0408e8
Mutuku1998 <martinmutuku1998@gmail.com>
2024-03-30 10:41:36 +0300

    m

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById("map"), options);
 
+    google.maps.event.addListener(map,"click",(event) =>{
+        addMarker({location:event.latLng});
+    })
+
+    // markers array
+
+   let markersArray = [
+        {location:{lat:-3.5107, lng: 39.9093},imageIcon:"https://img.icons8.com/nolan/2x/marker.png",content:'<div> <h3>You can get unlimited internet</h3><br/> <p> Mawingu is available in your area,please click the button below to request connection</p></div>'},
+
+
+        { location:{lat:1.2921, lng: 36.8219}},
+        {location:{lat:-4.0435, lng: 39.6682}},
+
+    ]
+
+    //loop marker
+     for(let i =0 ;i< markersArray.length;i++){
+     addMarker(markersArray[i]);
+
+     }
+
     // var marker = new google.maps.marker.AdvancedMarkerElement({
     //     position: {lat: 3.5107, lng: 39.9093},
     //     map: map,
@@ -42,17 +63,33 @@ function initMap() {
 //       map,
 //     });
 //   });
-function addMarker(location){
+function addMarker(props){
   const marker = new google.maps.Marker({
-   position: location,
+   position:props.location,
     map,
-   icon: "https://img.icons8.com/nolan/2x/marker.png",
+   icon: props.imageIcon,
    });
 
-}
-addMarker({lat:-3.5107, lng: 39.9093});
-addMarker({lat:1.2921, lng: 36.8219});
-addMarker({lat:-4.0435, lng: 39.6682});
+   if(props.imageIcon){
+    marker.setIcon(props.imageIcon)
+   }
+   if(props.content){
+    
+    const infowindow = new google.maps.InfoWindow({
+        content: props.content,
+        
+      });
+      marker.addListener("mouseover", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+    });
+   }
 
+}
+// addMarker();
+// addMarker();
+// addMarker();
 
 }
